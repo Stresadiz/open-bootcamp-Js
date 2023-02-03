2bfb359cff7b294254f3e21fc8f12da348f6f539
Sebastian Luna <eliaslunaslo@gmail.com>
2023-02-03 13:12:36 -0300

    Excercise 4

---
+++ sesion04-strings/ejercicio4.js
@@ -0,0 +1,21 @@
+let name = "Sebastian";
+
+let surname = "Luna";
+
+let estudiante = name.concat(` ${surname}`);
+
+let estudianteMayus = estudiante.toUpperCase();
+
+let estudianteMinus = estudiante.toLowerCase();
+
+let n = estudiante.length;
+
+let firstLetter  = name.charAt(0);
+
+let lastLetter = surname.charAt(length + 2);
+
+let spaces = estudiante.replace(" ", "");
+
+let boolean = estudiante.includes(name);
+
+console.log(boolean);
